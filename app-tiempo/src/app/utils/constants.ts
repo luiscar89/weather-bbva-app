2d9fb31eae715d134f5e447dd1db380e0c9d993a
luiscar89 <109737781+luiscar89@users.noreply.github.com>
2023-08-16 13:18:59 +0200

    Delete app-tiempo directory

--- app-tiempo/src/app/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SAVE_WEATHER_CITY_KEY = 'savedWeatherCities';
-export const SELECTED_CITY_KEY = 'selectedCity';
-export const NO_INTERNET_ERROR = 'HttpErrorResponse';
--- app-tiempo/src/app/utils/weatherIcon.map.ts
+++
@@ -1,7 +0,0 @@
-export const WEATHER_ICON_MAP: Map<string, string> = new Map([
-    ['Clear', 'light_mode'],
-    ['Clouds', 'filter_drama'],
-    ['Rain', 'water_drop'],
-    ['Haze', 'dehaze'],
-    ['no data', 'question_mark'],
-])
